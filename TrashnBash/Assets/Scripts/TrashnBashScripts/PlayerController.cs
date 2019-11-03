156e1f5ad980fda327b1fa74024de1a78efa47bc
Justin Tim <tim.justin8@gmail.com>
2019-11-03 01:43:49 -0700

    [TrashnBash]

---
+++ TrashnBash/Assets/Scripts/TrashnBashScripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+
+    [SerializeField]
+    private float moveSpeed = 100;
+    [SerializeField]
+    private float turnSpeed = 5f;
+    private float jumpSpeed = 5.0f;
+    private float gravity = 20.0f;
+    public bool isGrounded = true;
+
+    private void Awake()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(horizontal, 0, vertical); ;
+
+        if (Input.GetButtonDown("Jump") )
+        {
+            move.y = jumpSpeed;
+            isGrounded = false;
+        }
+
+        move.y -= gravity * Time.deltaTime;
+        controller.Move(move * Time.deltaTime * moveSpeed);
+
+        var movement = new Vector3(horizontal, 0, vertical);
+        if (movement.magnitude > 0)
+        {
+            Quaternion newDirection = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+        }
+    }
+
+
+}

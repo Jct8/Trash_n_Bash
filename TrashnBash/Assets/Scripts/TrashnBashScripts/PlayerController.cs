f58d8217a256c237c01bd3d8bbf5b44ce1d32503
JinMinJang <1820109@AIV.EDMC.STU>
2019-11-07 12:09:57 -0800

    Jimmy is genius test

--- TrashnBash/Assets/Scripts/TrashnBashScripts/PlayerController.cs
+++ TrashnBash/Assets/Scripts/TrashnBashScripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour, ICharacterAction
 {
     private CharacterController controller;
-
+	//Jimmy is genius
     [SerializeField]
     private float moveSpeed = 100;
     [SerializeField]
